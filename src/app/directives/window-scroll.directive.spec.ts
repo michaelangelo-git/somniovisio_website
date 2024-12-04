2122c0e995cc64051511da73196c87fc237436dc
Michael Lostritto <michaellostrittojr@gmail.com>
2024-12-04 15:44:49 -0500

    Revert "initial app upload"
    This reverts commit 72a87eee2355ef0611a79acb3d2e0f7f4d31b0ad.

--- src/app/directives/window-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WindowScrollDirective } from './window-scroll.directive';
-
-describe('WindowScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WindowScrollDirective(this);
-    expect(directive).toBeTruthy();
-  });
-});
